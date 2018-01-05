4ab033011cc04d1294ee9e568943be28c0b142d2
Tobias Schlemmer <keinstein@users.sourceforge.net>
2018-01-05 09:37:56 +0100

    dh_make

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(mingw-w64-libtiff):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="mingw-w64-libtiff" command="/usr/bin/mingw-w64-libtiff"
---
+++ debian/shlibs.local.ex
@@ -0,0 +1,1 @@
+libmingw-w64-libtiff 4.0.8 mingw-w64-libtiff (>> 4.0.8-0), mingw-w64-libtiff (<< 4.0.8-99)
